ffe675df58927437d1dd9a47cf28b3b0bebebc00
jculver1 <jculver1900@gmail.com>
2019-05-07 21:07:21 -0600

    added child component

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,24 +1,28 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
-/** BearController.getAllBears() Apex method */
-import getAllBears from '@salesforce/apex/BearController.getAllBears';
+/** BearController.searchBears(searchTerm) Apex method */
+import searchBears from '@salesforce/apex/BearController.searchBears';
+import { loadStyle } from 'lightning/platformResourceLoader';
 
 export default class BearList extends LightningElement {
-	@track bears;
-	@track error;
-	appResources = {
-		bearSilhouette: ursusResources +'/img/standing-bear-silhouette.png',
-	};
+	@track searchTerm = '';
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	bears;
 	connectedCallback() {
-		this.loadBears();
+		loadStyle(this, ursusResources + '/style.css');
 	}
-	loadBears() {
-		getAllBears()
-			.then(result => {
-				this.bears = result;
-			})
-			.catch(error => {
-				this.error = error;
-			});
+	handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation 	 
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+	get hasResults() {
+		return (this.bears.data.length > 0);
 	}
 }
