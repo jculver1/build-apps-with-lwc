91b7c2ea365a46737e84911e86431ba319da6205
jculver1 <jculver1900@gmail.com>
2019-05-08 15:26:52 -0600

    complete bear tracker

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,13 +1,21 @@
-import { LightningElement, track, wire } from 'lwc';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
+import { loadStyle } from 'lightning/platformResourceLoader';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
+import { LightningElement, track, wire } from 'lwc';
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
-import { loadStyle } from 'lightning/platformResourceLoader';
-
-export default class BearList extends LightningElement {
+export default class BearListNav extends NavigationMixin(LightningElement) {
 	@track searchTerm = '';
+	@track bears;
+	@wire(CurrentPageReference) pageRef;
 	@wire(searchBears, {searchTerm: '$searchTerm'})
-	bears;
+	loadBears(result) {
+		this.bears = result;
+		if (result.data) {
+			fireEvent(this.pageRef, 'bearListUpdate', result.data);
+		}
+	}
 	connectedCallback() {
 		loadStyle(this, ursusResources + '/style.css');
 	}
@@ -17,12 +25,25 @@ export default class BearList extends LightningElement {
 		// This is to avoid a very large number of Apex method calls.
 		window.clearTimeout(this.delayTimeout);
 		const searchTerm = event.target.value;
-		// eslint-disable-next-line @lwc/lwc/no-async-operation 	 
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
 		this.delayTimeout = setTimeout(() => {
 			this.searchTerm = searchTerm;
 		}, 300);
 	}
 	get hasResults() {
 		return (this.bears.data.length > 0);
+	}
+	handleBearView(event) {
+		// Get bear record id from bearview event
+		const bearId = event.detail;
+		// Navigate to bear record page
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: bearId,
+				objectApiName: 'Bear__c',
+				actionName: 'view',
+			},
+		});
 	}
 }
